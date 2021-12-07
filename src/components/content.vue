3b10c7a0287156f94851243c28c571d64e458652
hjxhjjhdc <1105215562@qq.com>
2021-12-07 19:33:58 +0800

    myContent

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,5 +1,23 @@
 <template>
-  
+  <div style=" padding: 20px">
+    <a-row :gutter="16">
+      <a-col :span="5">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="14">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="5">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,20 +1,35 @@
 <template>
- <a-layout>
-    <a-layout-header><Header /></a-layout-header>
-    <a-layout-content><Content /></a-layout-content>
-    <a-layout-footer><Footer /></a-layout-footer>
+  <a-layout>
+    <a-layout-header class="layoutContent">
+      <Header class="myContent"/>
+    </a-layout-header>
+    <a-layout-content class="layoutContent">
+      <MyContent class="myContent"/>
+    </a-layout-content>
+    <a-layout-footer>
+      <Footer/>
+    </a-layout-footer>
   </a-layout>
 </template>
 
 <script>
 import Header from '@/components/header.vue';
-import Content from '@/components/content.vue';
+import MyContent from '@/components/content.vue';
 import Footer from '@/components/footer.vue';
+
 export default {
-  components:{Header,Content,Footer}
+  components: {Header, MyContent, Footer}
 };
 </script>
 
 <style lang="less" scoped>
+.layoutContent {
+  display: flex;
+  justify-content: center;
+
+  .myContent {
+    width: 80%;
+  }
+}
 
 </style>
